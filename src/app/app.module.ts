7f1e0ec1336fa1c258b15b169e671c4cfb6f3470
Melisa500 <80432492+Melisa500@users.noreply.github.com>
2022-07-28 17:27:55 -0300

    Feature/frontend (#1)
    * the basic structure was created
    
    * bootstrap was installed
    
    * create component header
    
    * the nav was created
    
    * the banner was created and about me
    
    * generate componets jobs
    
    * structure was given to the work section
    
    * create component education
    
    * all the content of the education section was created
    
    * create component skills
    
    * create component skills
    
    * all the content of the skills section was created
    
    * project sector completed
    
    * create component footer
    
    * the footer sector is completed
    
    * se agregaron @medias
    
    * modificaciones en el front

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { JobsComponent } from './components/jobs/jobs.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    JobsComponent,
+    EducationComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
